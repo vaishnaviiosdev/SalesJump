692b193a8bd3392fcfac31f10ae150590e4f61ca
Mani <manisaneforce@gmail.com>
2026-08-21 18:06:41 +0530

    Add Record API in My Day Plan

---
+++ Common/GlobalFunc.swift
@@ -0,0 +1,8 @@
+//
+//  GlobalFunc.swift
+//  SalesJump
+//
+//  Created by Saneforce on 21/08/26.
+//
+
+import Foundation
---
+++ Services/Loader.swift
@@ -0,0 +1,18 @@
+//
+//  Loader.swift
+//  SalesJump
+//
+//  Created by Saneforce on 21/08/26.
+//
+
+import SwiftUI
+
+struct Loader: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Loader()
+}
